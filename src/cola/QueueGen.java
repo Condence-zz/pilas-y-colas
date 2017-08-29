851102ff4c37f3cb9633aed0c0ba25bd267c699c
Condence <coondee@hotmail.com>
2017-08-28 18:09:10 -0700

    update
    update

--- src/cola/QueueGen.java
+++ src/cola/QueueGen.java
@@ -9,38 +9,106 @@ public class QueueGen<E> {
         cola = (E[]) new Object[tamCola];
         front = -1;
         rear = -1;
-    }     
-    public boolean full(){ 
-        return rear == cola.length-1;
     }
+    /**
+     * 
+     * @return 
+     */
+    public boolean full() { 
+        if(rear==tamCola-1 && front==0 || rear+1==front){
+            return true; 
+        }
+        else{
+            return false; 
+        }
+    }
+    /**
+     * 
+     * @return 
+     */
     public boolean empty(){
-        return front == rear;
+        if(rear==-1 && front==-1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    public void enqueue(E elemento) throws QueueException {
-        if (!full()) { 
-            cola[++rear] = elemento;
-        } else {
+    /**
+     * 
+     * @param elemento
+     * @throws QueueException Si la cola esta llena 
+     */
+    public void enqueue(E elemento) throws QueueException { 
+        if(full()==true){
             throw new QueueException("La cola esta llena.. Elimine datos...");
-        }
-    }
-    public E dequeue() throws QueueException {
-        if (!empty()) {
-            return cola[++front];
         } else {
-            throw new QueueException("La cola esta vacia.. Inserte datos..."); 
+                if (empty()) {
+                front = 0;
+                rear = 0;
+            } else {
+                if (rear == tamCola - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            } 
+            cola[rear] = elemento;
+                
         }
     }
-    
-    public E peek(){
-        if (!empty()) {
-            return cola[0];
+    /**
+     * 
+     * @return
+     * @throws QueueException 
+     */
+    public E dequeue() throws QueueException { 
+        E aux = null;
+        if(empty()==true){ 
+            throw new QueueException("La cola esta vacia.. Inserte datos..."); 
+        } else{ 
+            aux = cola[front];
+            if(front==rear){
+                front=-1;
+                rear=-1;
+            }
+            else{
+                front++;
+            }
+        }
+        return aux; 
+    }
+    /**
+     * 
+     * @return
+     * @throws QueueException 
+     */
+    public E peek() throws QueueException {
+        if (empty()) {
+            return cola[front -1];
         } else {
             throw new QueueException("La cola esta vacia.. Inserte datos...");
         } 
     }
     @Override
     public String toString() {
-        return "QueueGen{" + "cola=" + cola + ", front=" + front + ", rear=" + rear + '}';
+        String cadena = "";
+        int actual = front;
+        if (!empty()) {
+            while (actual != rear) {
+
+                cadena = cadena + cola[actual] + ", ";
+
+                if (actual == tamCola - 1) {
+                    actual = 0;
+                } else {
+                    actual++;
+                }
+            }
+            cadena = cadena + cola[rear];
+        }
+     
+        return cadena;
     }
     
 }
--- src/pruebas/PruebaQueueGen.java
+++ src/pruebas/PruebaQueueGen.java
@@ -1,11 +1,152 @@
 package pruebas; 
 
 import cola.QueueGen; 
-import java.util.Stack;
+import excepciones.QueueException;
+import java.util.Stack; 
 public class PruebaQueueGen { 
-    public static void main(String[] args) {   
-        QueueGen queue = new QueueGen(5);    
-        System.out.println(queue.peek()); 
+    public static void main(String[] args) throws QueueException {
+        String errorMessage = null;
+        
+        // Cree una instancia de una cola circular para guardar enteros, de tamaño 5
+        QueueGen queue = new QueueGen(5);  
+        
+        // Agregue los siguientes valores a la cola: 1, 2, 3. 
+        try { 
+            queue.enqueue(1);
+            System.out.println(queue);
+            queue.enqueue(2);
+            System.out.println(queue);
+            queue.enqueue(3);
+            System.out.println(queue);
+        } catch(QueueException e) {
+            System.out.println(e);
+        } 
+        
+        // Extraiga dos valores de la cola 
+        try { 
+            queue.dequeue();
+            System.out.println(queue);  
+            queue.dequeue();
+            System.out.println(queue);  
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+        
+        // Agregue los siguientes valores a la cola: 4, 5.
+        try { 
+            queue.enqueue(4);
+            System.out.println(queue); 
+            queue.enqueue(5); 
+            System.out.println(queue);
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+        
+        // Extraiga cuatro valores de la cola 
+        try { 
+            queue.dequeue();
+            System.out.println(queue);
+            queue.dequeue();
+            System.out.println(queue);
+            queue.dequeue();
+            System.out.println(queue);
+            queue.dequeue();
+            System.out.println(queue);
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+        
+        // Agregue los siguientes valores a la cola: 6, 7, 8, 9, 10, 11. 
+        try { 
+            queue.enqueue(6); 
+            System.out.println(queue);
+            queue.enqueue(7); 
+            System.out.println(queue);
+            queue.enqueue(8); 
+            System.out.println(queue);
+            queue.enqueue(9); 
+            System.out.println(queue);
+            queue.enqueue(10); 
+            System.out.println(queue);
+            queue.enqueue(11); 
+            System.out.println(queue);  
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+        System.out.println("----------------------------");
+        // Cree una instancia de una cola circular para guardar cadenas, de tamaño 4 
+        QueueGen queue2 = new QueueGen(4);  
+        
+        // Agregue los siguientes valores a la cola: “A”, “B”.  
+        try { 
+            queue2.enqueue("A");
+            System.out.println(queue2);
+            queue2.enqueue("B");
+            System.out.println(queue2); 
+        } catch(QueueException e) {
+            System.out.println(e);
+        } 
+        
+        // Extraiga tres valores de la cola 
+        try { 
+            queue2.dequeue();
+            System.out.println(queue2);
+            queue2.dequeue();
+            System.out.println(queue2);
+            queue2.dequeue();
+            System.out.println(queue2); 
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+
+        // Agregue los siguientes valores a la cola: “C”, “D”, “E”, “F”.  
+        try { 
+            queue2.enqueue("C");
+            System.out.println(queue2);
+            queue2.enqueue("D");
+            System.out.println(queue2); 
+            queue2.enqueue("E");
+            System.out.println(queue2); 
+            queue2.enqueue("F");
+            System.out.println(queue2); 
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+        System.out.println("----------------------------");
+        // Cree una instancia de una cola circular de tipo crudo, de tamaño 4 
+        QueueGen queue3 = new QueueGen(4);  
+        
+        // Agregue los siguientes valores a la cola: 1, “A”, 2.  
+        try { 
+            queue3.enqueue(1);
+            System.out.println(queue3);
+            queue3.enqueue("A");
+            System.out.println(queue3);
+            queue3.enqueue(2);
+            System.out.println(queue3); 
+        } catch(QueueException e) {
+            System.out.println(e);
+        } 
+        
+        // Extraiga dos valores de la cola 
+        try { 
+            queue3.dequeue();
+            System.out.println(queue3);
+            queue3.dequeue();
+            System.out.println(queue3); 
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
+
+        // Agregue los siguientes valores a la cola: “B”, “C”.  
+        try { 
+            queue3.enqueue("B");
+            System.out.println(queue3);
+            queue3.enqueue("C");
+            System.out.println(queue3);  
+        } catch(QueueException e) {
+            System.out.println(e);
+        }
          
     } 
 }
